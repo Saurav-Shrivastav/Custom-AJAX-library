3cf20fc5c0739eabd24b3d3ac5f9f9e0277de150
Saurav <sauravsrivastav103@gmail.com>
2020-03-27 16:36:15 +0530

    Now we can get single posts as well using the id of the post.

--- app.js
+++ app.js
@@ -9,3 +9,13 @@ http.get('https://jsonplaceholder.typicode.com/posts', function(err, posts){
     console.log(posts);
   }
 });
+
+// GET Single Post:
+http.get('https://jsonplaceholder.typicode.com/posts/1', function(err, post){
+  if(err){
+    console.log(err);
+  }
+  else{
+    console.log(post);
+  }
+});         //The last number in the url here specifies the id of the post that we need.
